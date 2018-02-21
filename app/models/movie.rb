41fe952d2c388ca858cbcb296e1f0b9494db63a2
root <root@ip-172-31-34-206.us-east-2.compute.internal>
2018-02-21 00:16:28 +0000

    Part 3 100% complete

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -2,7 +2,7 @@ class Movie < ActiveRecord::Base
     #belongs_to :rating
     
     def self.getratings
-       movie_ratings = ['G', 'PG', 'PG-13', 'R']
+       movie_ratings = ['G', 'PG', 'PG-13', 'NC-17', 'R']
        movie_ratings
     end
 end
